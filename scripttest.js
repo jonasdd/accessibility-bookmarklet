901aac07be08ef13167e64de92ea7bed7931866a
jonasdd <jonas.brecht@gmail.com>
2016-11-23 09:08:54 +0100

    Update scripttest.js

--- scripttest.js
+++ scripttest.js
@@ -5,20 +5,14 @@
     
     var styleSheetArray = [url1, url2, url3];
     // on cree le select
-    var body = document.getElementsByTagName('body')[0];
-    var select = document.createElement('SELECT');
-    document.body.appendChild(select);
-    select.setAttribute("id","monSelect");
-    select.setAttribute("onchange","loadCss(event);");
-    select.setAttribute("style","position:fixed;background:white;bottom:0;");
+    //var body = document.getElementsByTagName('body')[0];
+   // var select = document.createElement('SELECT');
+   // document.body.appendChild(select);
+    //select.setAttribute("id","monSelect");
+    //select.setAttribute("onchange","loadCss(event);");
+    //select.setAttribute("style","position:fixed;background:white;bottom:0;");
     select1 = document.getElementById('monSelect');
-    // on ajoute autant d'option de css
-    styleSheetArray.forEach(function(element) {
-        var filename = element.substring(element.lastIndexOf("/") + 1, element.lastIndexOf("."));
-        var opt = document.createElement('option');
-        opt.innerHTML = filename;
-        select1.appendChild(opt);
-    });
+
     function loadCss(event) {
       var selectedCss = event.target.value;
       var selectedArrPos = styleSheetArray.indexOf('https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/'+selectedCss+'.css');
