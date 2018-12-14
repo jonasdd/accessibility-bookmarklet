6331d1d1eb0fe5932af0ba6fc5d6aa94b1d78f06
jonasdd <jonas.brecht@gmail.com>
2018-12-14 09:02:59 +0100

    Update script bookmarklet 0.3
    modification des chemins rawgit > jsdelivr

--- script-bookmarklet-rgaa-0.3.js
+++ script-bookmarklet-rgaa-0.3.js
@@ -1,19 +1,19 @@
 // on liste les urls des stylesheet et on les mets dans un tableau
-    var url1 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-aide-saisie.css",
-    url2 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-focus.css",
-    url3 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-heading.css",
-    url4 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-indication-erreur-AA.css",
-    url5 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-indication-erreur.css",
-    url6 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-indication-obligatoire.css",
-    url7 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-jargon.css",
-    url8 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-label-IE.css",
-    url9 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-label.css",
-    url10 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-landmark-aria.css",
-    url11 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-list.css",
-    url12 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check-target-new-window.css",
-    url13 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check_hidden_content.css",
-    url14 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check_presentation.css",
-    url15 = "https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/check_structure.css";
+    var url1 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-aide-saisie.css",
+url2 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-focus.css",
+url3 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-heading.css",
+url4 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-indication-erreur-AA.css",
+url5 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-indication-erreur.css",
+url6 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-indication-obligatoire.css",
+url7 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-jargon.css",
+url8 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-label-IE.css",
+url9 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-label.css",
+url10 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-landmark-aria.css",
+url11 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-list.css",
+url12 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check-target-new-window.css",
+url13 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check_hidden_content.css",
+url14 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check_presentation.css",
+url15 = "https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/check_structure.css";
 
     var styleSheetArray = [url1, url2, url3, url4, url5, url6, url7, url8, url9, url10, url11, url12, url13, url14, url15],
     body = document.getElementsByTagName('body')[0],
@@ -22,7 +22,7 @@
 
     function loadCss(event) {
       var selectedCss = event.target.value;
-      var selectedArrPos = styleSheetArray.indexOf('https://cdn.rawgit.com/DISIC/rgaa_methodologie/master/Check-css/'+selectedCss+'.css');
+      var selectedArrPos = styleSheetArray.indexOf('https://cdn.jsdelivr.net/gh/DISIC/rgaa_methodologie@master/Check-css/'+selectedCss+'.css');
       var url =  styleSheetArray[selectedArrPos];
       if (currentCss){
         element = document.getElementById('addedCss');
